9dc0924710f583f792668acce65d5f83959e9674
Timoterr <103418003+Timoterr@users.noreply.github.com>
2022-04-11 11:38:22 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,14 @@
+let n = 0
+while (!(input.buttonIsPressed(Button.A))) {
+    if (true) {
+        if (true) {
+            if (true) {
+            	
+            } else {
+                if (true) {
+                	
+                }
+            }
+        }
+    }
+}
